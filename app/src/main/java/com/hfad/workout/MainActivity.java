1a9e97c7ff35b4c053844dcae3e868b296d284b8
Aaron Harvey <harvnlenny@gmail.com>
2016-02-16 21:12:59 -0500

    Continue to link fragments. Now the workout details show up when the user selects a new workout from the list.

--- app/src/main/java/com/hfad/workout/MainActivity.java
+++ app/src/main/java/com/hfad/workout/MainActivity.java
@@ -1,15 +1,25 @@
 package com.hfad.workout;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements WorkoutListFragment.WorkoutListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WorkoutDetailFragment frag = (WorkoutDetailFragment) getFragmentManager().findFragmentById(R.id.detail_frag);
-        frag.setWorkoutId(1);
+    }
+
+    @Override
+    public void itemClicked(long id){
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        details.setWorkout(id);
+        ft.replace(R.id.fragment_container, details);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
     }
 }
